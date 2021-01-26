b021795b0672e9fc43d9447a7be135f17c8ee84a
Eduardo Serrano <eduardo.serrano@lexisnexis.co.uk>
2021-01-26 10:43:04 +0000

    update docs 5

--- demos/DotNet.Sdk.Extensions.Testing.Demos/HostedServices/HostedServicesWebApplicationFactory.cs
+++ demos/DotNet.Sdk.Extensions.Testing.Demos/HostedServices/HostedServicesWebApplicationFactory.cs
@@ -6,6 +6,7 @@ namespace DotNet.Sdk.Extensions.Testing.Demos.HostedServices
 {
     // For more information on why this custom WebApplicationFactory<T> is configured as below
     // please see the doc at /docs/integration-tests/web-application-factory.md 
+    // You might NOT need to create a custom class that implements WebApplicationFactory
     public class HostedServicesWebApplicationFactory : WebApplicationFactory<StartupHostedService>
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
--- demos/DotNet.Sdk.Extensions.Testing.Demos/HttpMocking/HttpResponseMockingWebApplicationFactory.cs
+++ demos/DotNet.Sdk.Extensions.Testing.Demos/HttpMocking/HttpResponseMockingWebApplicationFactory.cs
@@ -6,6 +6,7 @@ namespace DotNet.Sdk.Extensions.Testing.Demos.HttpMocking
 {
     // For more information on why this custom WebApplicationFactory<T> is configured as below
     // please see the doc at /docs/integration-tests/web-application-factory.md 
+    // You might NOT need to create a custom class that implements WebApplicationFactory
     public class HttpResponseMockingWebApplicationFactory : WebApplicationFactory<StartupHttpResponseMocking>
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
