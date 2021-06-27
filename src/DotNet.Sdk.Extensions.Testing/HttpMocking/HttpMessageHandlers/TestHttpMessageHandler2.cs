a46eaa35fb488a8cb2c1ef9b4d56bf58435f09e6
Eduardo Serrano <eduardomserrano@outlook.com>
2021-06-27 13:40:33 +0100

    test code cov 2

---
+++ src/DotNet.Sdk.Extensions.Testing/HttpMocking/HttpMessageHandlers/TestHttpMessageHandler2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using DotNet.Sdk.Extensions.Testing.HttpMocking.HttpMessageHandlers.ResponseMocking;
+
+namespace DotNet.Sdk.Extensions.Testing.HttpMocking.HttpMessageHandlers
+{
+    /// <summary>
+    /// An implementation of <see cref="DelegatingHandler"/> that can be used for <see cref="HttpClient"/> testing.
+    /// </summary>
+    public class TestHttpMessageHandler2 : DelegatingHandler
+    {
+        private readonly List<HttpResponseMessageMock> _httpResponseMocks = new List<HttpResponseMessageMock>();
+
+        /// <summary>
+        /// Configure an <see cref="HttpResponseMessage"/> to be returned when executing an HTTP call via
+        /// an <see cref="HttpClient"/> configured to use the <see cref="TestHttpMessageHandler"/>.
+        /// </summary>
+        /// <param name="configure">Action to configure the <see cref="HttpResponseMessage"/> to be returned.</param>
+        /// <returns>The <see cref="TestHttpMessageHandler"/> for chaining.</returns>
+        public TestHttpMessageHandler2 MockHttpResponse(Action<HttpResponseMessageMockBuilder> configure)
+        {
+            if (configure is null) throw new ArgumentNullException(nameof(configure));
+
+            var httpResponseMockBuilder = new HttpResponseMessageMockBuilder();
+            configure(httpResponseMockBuilder);
+            var httpResponseMock = httpResponseMockBuilder.Build();
+            MockHttpResponse(httpResponseMock);
+            return this;
+        }
+
+        /// <summary>
+        /// Configure an <see cref="HttpResponseMessage"/> to be returned when executing an HTTP call via
+        /// an <see cref="HttpClient"/> configured to use the <see cref="TestHttpMessageHandler"/>.
+        /// </summary>
+        /// <param name="httpResponseMock">The <see cref="HttpResponseMessageMock"/> that defines the <see cref="HttpResponseMessage"/> that will be returned.</param>
+        /// <returns>The <see cref="TestHttpMessageHandler"/> for chaining.</returns>
+        public TestHttpMessageHandler2 MockHttpResponse(HttpResponseMessageMock httpResponseMock)
+        {
+            if (httpResponseMock is null) throw new ArgumentNullException(nameof(httpResponseMock));
+
+            _httpResponseMocks.Add(httpResponseMock);
+            return this;
+        }
+
+        /// <summary>
+        /// Executes the configured mock behavior for the http request instead of actually doing the http request.
+        /// </summary>
+        /// <param name="request">The <see cref="HttpRequestMessage"/> to be executed/</param>
+        /// <param name="cancellationToken">A cancellation token to cancel the operation</param>
+        /// <returns></returns>
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            foreach (var httpResponseMock in _httpResponseMocks)
+            {
+                var responseMockResult = await httpResponseMock.ExecuteAsync(request, cancellationToken);
+                if (responseMockResult.Status == HttpResponseMessageMockResults.Executed)
+                {
+                    return responseMockResult.HttpResponseMessage;
+                }
+            }
+            throw new InvalidOperationException($"No response mock defined for {request.Method} to {request.RequestUri}.");
+        }
+    }
+}
