2817985e3a0f15c569548be2776433ae448b3bfa
Eduardo Serrano <eduardo.serrano@lexisnexis.co.uk>
2021-09-05 15:58:50 +0100

    auto dotnet format 03-Sep-2021 (#108)
    Co-authored-by: edumserrano <edumserrano@users.noreply.github.com>

--- src/DotNet.Sdk.Extensions/Options/OptionsBuilderExtensions.cs
+++ src/DotNet.Sdk.Extensions/Options/OptionsBuilderExtensions.cs
@@ -22,7 +22,9 @@ namespace DotNet.Sdk.Extensions.Options
         public static OptionsBuilder<T> AddOptionsValue<T>(this IServiceCollection services, IConfiguration configuration)
             where T : class, new()
         {
-            if (services is null)            {                throw new ArgumentNullException(nameof(services));
+            if (services is null)
+            {
+                throw new ArgumentNullException(nameof(services));
             }
 
             return services
