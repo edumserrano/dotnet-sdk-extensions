ebb61ab03409cf74fa7b33a1542e4f666ada2786
Eduardo Serrano <eduardomserrano@outlook.com>
2021-10-06 02:42:21 +0100

    wip14

--- src/DotNet.Sdk.Extensions/Options/OptionsBuilderExtensions.cs
+++ src/DotNet.Sdk.Extensions/Options/OptionsBuilderExtensions.cs
@@ -22,8 +22,7 @@ namespace DotNet.Sdk.Extensions.Options
         public static OptionsBuilder<T> AddOptionsValue<T>(this IServiceCollection services, IConfiguration configuration)
             where T : class, new()
         {
-            if (services is null)
-            {
+            if (services is null)            {
                 throw new ArgumentNullException(nameof(services));
             }
 
