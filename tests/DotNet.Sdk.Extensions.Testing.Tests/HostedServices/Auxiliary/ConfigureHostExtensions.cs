8571c09da4420e132f57010c59c0c665e45892ba
Eduardo Serrano <eduardomserrano@outlook.com>
2021-12-17 13:58:35 +0000

    remove xunit config files

--- tests/DotNet.Sdk.Extensions.Testing.Tests/HostedServices/Auxiliary/ConfigureHostExtensions.cs
+++ tests/DotNet.Sdk.Extensions.Testing.Tests/HostedServices/Auxiliary/ConfigureHostExtensions.cs
@@ -11,7 +11,7 @@ namespace DotNet.Sdk.Extensions.Testing.Tests.HostedServices.Auxiliary
         /// </summary>
         /// <remarks>
         /// This should not have any impact on the tests. Its purpose is to avoid noise on the output logs when running dotnet test.
-        /// 
+        ///
         /// The exception log being disabled is as follows:
         ///
         /// crit: Microsoft.Extensions.Hosting.Internal.Host[10]
