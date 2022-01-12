c4add854f7731303607ea91d3c8e5b217c00eb43
edumserrano <edumserrano@users.noreply.github.com>
2022-01-12 01:49:47 +0000

    dotnet format for PR 235

--- tests/DotNet.Sdk.Extensions.Testing.Tests/Configuration/AddTestConfigurationHostTests.cs
+++ tests/DotNet.Sdk.Extensions.Testing.Tests/Configuration/AddTestConfigurationHostTests.cs
@@ -31,11 +31,6 @@ namespace DotNet.Sdk.Extensions.Testing.Tests.Configuration
                 .CreateDefaultBuilder()
                 .Build();
 
-
-                var a = 2;
-
-
-
             var configuration = (ConfigurationRoot)host.Services.GetRequiredService<IConfiguration>();
             var jsonConfigurationProviders = configuration.Providers.OfType<JsonConfigurationProvider>();
             jsonConfigurationProviders.Count().ShouldBe(2);
