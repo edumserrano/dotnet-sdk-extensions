3c9833970d7b7f5565a1988fada2208d6f6e4629
edumserrano <edumserrano@users.noreply.github.com>
2022-01-21 10:47:15 +0000

    dotnet format from workflow for PR 293

--- tests/DotNet.Sdk.Extensions.Testing.Tests/Configuration/TestConfigurationOptionsTests.cs
+++ tests/DotNet.Sdk.Extensions.Testing.Tests/Configuration/TestConfigurationOptionsTests.cs
@@ -5,7 +5,7 @@ using Xunit;
 namespace DotNet.Sdk.Extensions.Testing.Tests.Configuration
 {
     [Trait("Category", XUnitCategories.Configuration)]
-       public class TestConfigurationOptionsTests
+    public class TestConfigurationOptionsTests
     {
         /// <summary>
         /// Tests the default values for <see cref="TestConfigurationOptions"/>.
