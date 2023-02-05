a8612cdd51e8584414a12223ed951a6f77853b01
Eduardo Serrano <eduardomserrano@outlook.com>
2023-02-05 03:35:46 +0000

    update interface signature

--- tests/DotNet.Sdk.Extensions.Tests/Options/ValidateEagerly/Auxiliary/IValidateOptions/MyOptions1Validation.cs
+++ tests/DotNet.Sdk.Extensions.Tests/Options/ValidateEagerly/Auxiliary/IValidateOptions/MyOptions1Validation.cs
@@ -2,7 +2,7 @@ namespace DotNet.Sdk.Extensions.Tests.Options.ValidateEagerly.Auxiliary.IValidat
 
 public class MyOptions1Validation : IValidateOptions<MyOptions1>
 {
-    public ValidateOptionsResult Validate(string name, MyOptions1 options)
+    public ValidateOptionsResult Validate(string? name, MyOptions1 options)
     {
         // this is doing data annotation validation but you can implement the validation however you like
         Validator.ValidateObject(options, new ValidationContext(options), validateAllProperties: true);
