87769f74d82ac1c2db101c83823f81397251a723
Eduardo Serrano <eduardomserrano@outlook.com>
2023-08-10 14:53:03 +0100

    Detect unused usings when building via dotnet CLI

--- tests/DotNet.Sdk.Extensions.Testing.Tests/Configuration/AddTestConfigurationWebHostTests.cs
+++ tests/DotNet.Sdk.Extensions.Testing.Tests/Configuration/AddTestConfigurationWebHostTests.cs
@@ -4,9 +4,9 @@ namespace DotNet.Sdk.Extensions.Testing.Tests.Configuration;
 public class AddTestConfigurationWebHostTests
 {
     /// <summary>
-    /// Tests that <see cref="WebHost.CreateDefaultBuilder()"/> adds two <see cref="JsonConfigurationProvider"/>
+    /// Tests that <see cref="Microsoft.AspNetCore.WebHost.CreateDefaultBuilder()"/> adds two <see cref="JsonConfigurationProvider"/>
     /// to the app configuration.
-    /// This test serves as a control test because all the tests use the <see cref="WebHost.CreateDefaultBuilder()"/> as a way
+    /// This test serves as a control test because all the tests use the <see cref="Microsoft.AspNetCore.WebHost.CreateDefaultBuilder()"/> as a way
     /// to setup a <see cref="IWebHost"/> with several <see cref="ConfigurationProvider"/> and at least two <see cref="JsonConfigurationProvider"/>.
     /// If this changes in the future then I could start having false positives on the other tests.
     /// </summary>
