2364a1d94187194b21cf4786c9fac7cfb6a6ac38
Eduardo Serrano <eduardomserrano@outlook.com>
2023-08-13 16:58:32 +0100

    Remove OptionsBuilder<T>.ValidateEagerly()

--- tests/DotNet.Sdk.Extensions.Tests/Options/ValidateEagerly/Auxiliary/DataAnnotations/MyOptions2.cs
+++
@@ -1,7 +0,0 @@
-namespace DotNet.Sdk.Extensions.Tests.Options.ValidateEagerly.Auxiliary.DataAnnotations;
-
-public class MyOptions2
-{
-    [Required]
-    public string SomeOption { get; set; } = default!;
-}
